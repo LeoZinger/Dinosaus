9991e2d2dd4a26d2e96469677ed1fb9511095007
LeoZinger <leo.zinger@gmail.com>
2022-08-17 20:52:28 -0700

    Leetcode maxWidthBST.py - 2nd draft

--- maxWidthBST.py
+++ maxWidthBST.py
@@ -1,9 +1,10 @@
+import collections
 # Definition for a binary tree node.
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+    def __init__(self, val):
         self.val = val
-        self.left = left
-        self.right = right
+        self.left = None
+        self.right = None
 
 
 class Solution:
@@ -29,34 +30,45 @@ class Solution:
     def widthOfBinaryTree(self, root: TreeNode) -> int:
         if not root:
             return 0
-        height = self.findTreeHeight(root)
-        current_height = 1
-        queue = [root]
+        # height = self.findTreeHeight(root)
+        # current_height = 1
+        curr_q = [root]
+        next_q = []
         max_width = 0
-        bfs_list = []
-        while queue:
-            bfs_list.clear()
-            for i in range(len(queue)):
-                current_node = queue.pop(0)
-                # if current_node.val != float('inf'):
-                bfs_list.append(current_node.val)
-                if current_height < height:
-                    if not current_node.left:
-                        new_node = TreeNode(float('inf'))
-                        current_node.left = new_node
-                    if not current_node.right:
-                        new_node = TreeNode(float('inf'))
-                        current_node.right = new_node
-                middle = False
-                width = 0
-                for j in range(len(bfs_list)):
-                    if bfs_list[j] != float('inf'):
-                        middle = True
-                        width += 1
-                    if bfs_list[j] == float('inf') and middle:
-                        width += 1
-                max_width = max(max_width, width)
-                return max_width
+        while curr_q:
+            for i in range(len(curr_q)):
+                curr_node = curr_q[i]
+                if curr_node.left:
+                    # print("curr_node.left val : ", curr_node.left.val)
+                    next_q.append(curr_node.left)
+                if curr_node.right:
+                    # print("curr_node.left val : ", curr_node.right.val)
+                    next_q.append(curr_node.right)
+            # print("curr_q list type : ", type(curr_q))
+            # print("next_q list type : ", type(next_q))
+            for i in range(len(next_q)):
+                next_node = next_q[i]
+                print("next_q[", i, "] : ", next_node.val)
+            curr_width = 0
+            width_start = False
+            for curr_node in curr_q:
+                if (curr_node.left or curr_node.right) == next_q[0] \
+                        and not width_start:
+                    width_start = True
+                    curr_width += 1
+                if curr_node.left != next_q[len(next_q)-1] and curr_node.right != next_q[len(next_q)-1] \
+                        and width_start:
+                    curr_width += 2
+                if (curr_node.left == next_q[len(next_q)-1] or curr_node.right == next_q[len(next_q)-1]) \
+                        and width_start:
+                    curr_width += 1
+                    break
+            max_width = max(max_width, curr_width)
+
+            curr_q.clear()
+            curr_q = next_q
+            next_q.clear()
+        return max_width
 
         def findTreeHeight(node: TreeNode) -> int:
             if not node:
@@ -75,3 +87,4 @@ root.right.right = TreeNode(7)
 solution = Solution()
 # print("Height of tree = ", solution.findTreeHeight(root))
 solution.bfsTraverse(root)
+print("Max Width Of BinaryTree = ", solution.widthOfBinaryTree(root))
